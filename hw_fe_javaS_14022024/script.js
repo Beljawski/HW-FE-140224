730c4d2873ff6d1a063ae70cd385f54be167ae4e
Beljawski <155641687+Beljawski@users.noreply.github.com>
2024-02-17 16:24:42 +0100

    Add files via upload

---
+++ hw_fe_javaS_14022024/script.js
@@ -0,0 +1,61 @@
+// Создать кнопку и красный квадрат с размерами 200х200px. При клике на кнопку менять у квадрата цвет заднего фона на зеленый и уменьшать его до размеров 100х100px.
+
+// const square = document.querySelector (".square_1")
+// const btn = document.querySelector (".button_1")
+// btn.addEventListener ("click", function(){
+//   square.style.backgroundColor = "green"
+//   square.style.width = "100px";
+//   square.style.height = "100px";
+// })
+
+// Создать кнопку и розовый квадрат с размерами 200х200px. При клике на кнопку менять цвет на синий и выводить в консоль обновленный цвет квадрата.
+
+// const square = document.querySelector (".square_2")
+// const btn = document.querySelector (".button_2")
+// btn.addEventListener ("click", function(){
+//   square.style.backgroundColor = "blue"
+// console.log(square.style.backgroundColor = "blue");
+// })
+
+// Создать кнопку и квадрат с размерами 150х150px. При клике на кнопку увеличивать высоту и ширину квадрата на 20px.
+// const square = document.querySelector (".square_3")
+// const btn = document.querySelector (".button_3")
+// btn.addEventListener ("click", function(){
+//   let changeSize = 170
+//   square.style.width = changeSize + "px";
+//   square.style.height = changeSize + "px"
+// })
+
+
+// Создать кнопку и div с классом root. При клике на кнопку в div создается параграф синего цвета. Текст параграфа произвольный.
+
+// const container = document.querySelector(".root")
+// const but = document.querySelector(".button_4")
+
+// but.addEventListener("click", function(){
+// const parag = document.createElement ("p")
+// container.appendChild(parag)
+// parag.style.backgroundColor = "blue"
+// parag.innerText = "Paragraph hat die Farbe blau"
+// parag.style.color = "white"
+// })
+
+// Создать кнопку и div с классом root. При клике на кнопку в div создаются по очереди параграфы синего и зеленого цветов. Первый цвет синий.
+
+// const container = document.querySelector(".root_2")
+// const but = document.querySelector(".button_5")
+
+// but.addEventListener("click", function(){
+// const paragBlue = document.createElement ("p")
+// const paragGreen = document.createElement ("p")
+
+// paragBlue.innerText = "Paragraph hat die Farbe blau";
+// paragGreen.innerText = "Paragraph hat die Farbe grün";
+// paragBlue.style.backgroundColor = "blue"
+// paragGreen.style.backgroundColor = "green"
+// container.appendChild(paragBlue)
+// container.appendChild(paragGreen)
+// }
+// )
+
+
